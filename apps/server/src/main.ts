62592281e8b15545b98ef2eab14121d8df1dc48c
Joumy <Andrei-Alex@outlook.com>
2022-11-24 17:43:35 +0200

    Security Header

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,17 +1,29 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
+import helmet from 'helmet';
+import { ConfigService } from '@nestjs/config';
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  const configService = app.get(ConfigService);
   const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
+  app.use(helmet());
+  app.use(
+    helmet({
+      crossOriginEmbedderPolicy: false,
+    })
+  );
+  app.use(helmet.frameguard({ action: 'deny' }));
+
+  app.enableCors({
+    origin: [
+      configService.get('ALLOWED_ORIGIN1'),
+      configService.get('ALLOWED_ORIGIN2'),
+    ],
+    methods: ['POST', 'PUT', 'DELETE', 'GET'],
+  }),
+    app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
