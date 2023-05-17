c0b67e488f3343745277cd9318dda814e9595647
Andrei <Andrei-Alex@outlook.com>
2023-05-17 20:40:26 +0300

    update storybook

--- packages/client/src/components/ConfirmMessage/ConfirmMessage.test.tsx
+++ packages/client/src/components/ConfirmMessage/ConfirmMessage.test.tsx
@@ -11,14 +11,14 @@ describe("ConfirmMessage", () => {
       .toJSON();
     expect(tree).toMatchSnapshot();
   });
-  it("should display success", () => {
+  it("should display Success !!! and have CSS class success", () => {
     render(<ConfirmMessage success={"Success !!!"} />);
     const message = screen.getByText("Success !!!");
 
     expect(message).toBeInTheDocument();
     expect(message).toHaveClass("success");
   });
-  it("should display error", () => {
+  it("should display Bad Credentials and have CSS class error", () => {
     render(<ConfirmMessage error={"Bad Credentials !!!"} />);
     const message = screen.getByText("Bad Credentials !!!");
 
