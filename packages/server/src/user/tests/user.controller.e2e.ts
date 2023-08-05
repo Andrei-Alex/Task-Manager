9bb5c4775730be756fbe97bf1b4f5579998622ec
Andrei-Alex <Andrei-Alex@outlook.com>
2023-08-05 15:12:53 +0300

    UnitTests updated

--- packages/server/src/user/tests/user.controller.e2e.ts
+++ packages/server/src/user/tests/user.controller.e2e.ts
@@ -5,12 +5,12 @@ import { INestApplication } from '@nestjs/common';
 import { AppModule } from '../../app.module';
 import * as jwt from 'jsonwebtoken';
 import { ConfigService } from '@nestjs/config';
+import { JwtAuthGuard } from '../../auth/jwt-auth.guard';
 
 const configService = new ConfigService();
 const random = Math.floor(Math.random() * 1283);
 describe('AuthController (e2e)', () => {
   let app: INestApplication;
-
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
@@ -18,9 +18,12 @@ describe('AuthController (e2e)', () => {
     app = moduleFixture.createNestApplication();
     await app.init();
   });
+  afterAll(async () => {
+    await app.close();
+  });
 
   describe('authentication/login (POST)', () => {
-    it('should create new user', () => {
+    it('should create new user and return full_name, email but not password', () => {
       return request(app.getHttpServer())
         .post('/auth/register')
         .set('Content-Type', 'application/json')
@@ -29,10 +32,19 @@ describe('AuthController (e2e)', () => {
           email: `johnyBeGood${random}@mail.com`,
           password: 'changeMe',
         })
-        .expect(HttpStatus.CREATED);
+        .expect(HttpStatus.CREATED)
+        .then((response: request.Response) => {
+          const email = response.body.email.toLowerCase();
+          expect(response.body).not.toHaveProperty('password');
+          expect(response.body).toHaveProperty('full_name');
+          expect(response.body).toHaveProperty('email');
+          expect(response.body.full_name).toEqual('Johny BeGood');
+          expect(email).toEqual(expect.stringContaining('johnybegood'));
+          expect(email).toEqual(expect.stringContaining('@mail.com'));
+        });
     });
 
-    it('should not log in nor return a JWT for an unregistered user', () => {
+    it('should not log in nor return a JWT for an UNAUTHORIZED user (401)', () => {
       return request(app.getHttpServer())
         .post('/auth/login')
         .set('Content-Type', 'application/json')
@@ -44,29 +56,91 @@ describe('AuthController (e2e)', () => {
         .expect(HttpStatus.UNAUTHORIZED);
     });
 
-    it('should not log in nor return a JWT for an unregistered user', () => {
+    it('should not log in nor return a JWT for an BAD_REQUEST (400)', () => {
       return request(app.getHttpServer())
         .post('/auth/login')
         .set('Content-Type', 'application/json')
         .send({ username: 'John' })
-        .expect((response: request.Response) => {
+        .expect(HttpStatus.BAD_REQUEST)
+        .then((response: request.Response) => {
           const { token }: { token: string } = response.body;
           expect(token).toBeUndefined();
-        })
-        .expect(HttpStatus.BAD_REQUEST);
+        });
     });
 
-    it('should log in and return a JWT for a registered user', () => {
+    it('should log in and return a valid JWT for a registered user', () => {
       return request(app.getHttpServer())
         .post('/auth/login')
         .set('Content-Type', 'application/json')
         .send({ username: 'johnyJohny@mail.com', password: 'changeMe' })
-        .expect((response: request.Response) => {
-          const token = response.body.access_token;
+        .expect(201)
+        .then((response: request.Response) => {
+          expect(response.body).toHaveProperty('access_token');
           expect(
-            jwt.verify(token, configService.get('SECRET_KEY')),
+            jwt.verify(
+              response.body.access_token,
+              configService.get('SECRET_KEY'),
+            ),
           ).toBeTruthy();
         });
     });
+    it('should return access_token and login', () => {
+      return request(app.getHttpServer())
+        .post('/auth/login')
+        .set('Content-Type', 'application/json')
+        .send({ username: 'johnyJohny@mail.com', password: 'changeMe' })
+        .expect(201)
+        .then((response: request.Response) => {
+          expect(response.body).toHaveProperty('access_token');
+          expect(
+            jwt.verify(
+              response.body.access_token,
+              configService.get('SECRET_KEY'),
+            ),
+          ).toBeTruthy();
+        });
+    });
+  });
+});
+
+describe('UserController (e2e)', () => {
+  let app: INestApplication;
+
+  beforeAll(async () => {
+    const moduleFixture = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideGuard(JwtAuthGuard)
+      .useValue({ canActivate: () => true })
+      .compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it('/api/users (GET) should return all users', async () => {
+    const response = await request(app.getHttpServer()).get('/auth/users');
+
+    expect(response.status).toBe(200);
+    console.log(response.body);
+    expect(Array.isArray(response.body)).toBe(true);
+    expect(response.body.length).toBeGreaterThan(0);
+  });
+
+  it('/api/users/:full_name (GET) should return users by name', async () => {
+    const fullName = 'Johny BeGood';
+    const response = await request(app.getHttpServer()).get(
+      `/auth/users/${fullName}`,
+    );
+    expect(response.status).toBe(200);
+    expect(response.status).toBe(200);
+    expect(typeof response.body).toBe('object');
+    expect(response.body).toHaveProperty('id');
+    expect(response.body).toHaveProperty('full_name');
+    expect(response.body).toHaveProperty('email');
   });
 });
