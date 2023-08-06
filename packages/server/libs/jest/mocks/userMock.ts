d74714590478bfa12c015ead6a2f290498444cc4
Andrei-Alex <Andrei-Alex@outlook.com>
2023-08-06 11:40:11 +0300

    UnitTests updated

--- packages/server/libs/jest/mocks/userMock.ts
+++ packages/server/libs/jest/mocks/userMock.ts
@@ -1,13 +1,21 @@
 import { UserService } from '../../../src/user/user.service';
 import { User } from '../../../src/user/User.entity';
+import { AuthService } from '../../../src/auth/auth.service';
 
-export let users: User[] = [];
 export const mockedUser = {
   id: 67,
   full_name: 'Jest',
   password: 'unit-test',
   email: `jest@mail.com`,
 };
+export const mockedUser2 = {
+  id: 7,
+  full_name: 'Andrei',
+  password: 'unit-test',
+  email: `andrei@mail.com`,
+  boards: [],
+};
+export let users: User[] = [mockedUser2];
 
 export const fakeUserService: Partial<UserService> = {
   findAll: () => Promise.resolve(users),
@@ -26,5 +34,20 @@ export const fakeUserService: Partial<UserService> = {
     } as User;
     users = [...users, user];
     return Promise.resolve(user);
+  },
+};
+export const fakeAuthService: Partial<AuthService> = {
+  validateUser: (email: string, password: string): Promise<User> => {
+    if (email && password) {
+      const [user] = users.filter(
+        (user) => user.email === email && user.password === password,
+      );
+      if (user) {
+        delete user.password;
+        return Promise.resolve(user as User);
+      }
+      return Promise.resolve(null);
+    }
+    return Promise.resolve(null);
   },
 };
