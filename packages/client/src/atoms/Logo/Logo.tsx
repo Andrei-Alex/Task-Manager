ed1a547d46c25e1d9e4bfdd65253e0f8b6344cd8
Andrei-Alex <Andrei-Alex@outlook.com>
2023-08-14 20:04:47 +0300

    Refactored pages and services

--- packages/client/src/atoms/Logo/Logo.tsx
+++ packages/client/src/atoms/Logo/Logo.tsx
@@ -1,3 +1,9 @@
-const Logo = () => {};
+import React from "react";
+
+const Logo = ({ logo }) => {
+  return (
+    <div>{logo.src ? <image href={logo.src} /> : <h1>{logo.name}</h1>}</div>
+  );
+};
 
 export default Logo;
