b0fb6abb17647f0e1d4a74a7bfc93a92d34adaa6
Andrei <Andrei-Alex@outlook.com>
2023-08-15 17:07:53 +0300

    Refactoring Health

---
+++ packages/client/src/atoms/Logo/Logo.stories.tsx
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import React from "react";
+import Logo from "./Logo";
+
+const meta: Meta<typeof Logo> = {
+  component: Logo,
+  title: "Atoms/Logo",
+  tags: ["autodocs"],
+  argTypes: {},
+};
+export default meta;
+type Story = StoryObj<typeof Logo>;
+
+export const Default: Story = {
+  args: {
+    logo: { name: "Kanban" },
+    appData: {
+      clientVersion: "1.6.8",
+      server: { dbName: "dev", serverVersion: "1.9.6" },
+    },
+  },
+};
