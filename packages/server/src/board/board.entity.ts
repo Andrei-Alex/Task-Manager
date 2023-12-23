a448a4b60c471d9e9d7facc5bb2445471fdc9df5
Andrei-Alex <Andrei-Alex@outlook.com>
2023-12-23 10:56:52 +0200

    Docs added

--- packages/server/src/board/board.entity.ts
+++ packages/server/src/board/board.entity.ts
@@ -9,24 +9,43 @@ import {
 } from 'typeorm';
 import { User } from '../user/User.entity';
 import { BoardColumn } from '../column/column.entity';
+import { ApiProperty } from '@nestjs/swagger'; // make sure to install Swagger related package
 
+/**
+ * Board entity representing a project board
+ */
 @Entity()
 export class Board {
+  @ApiProperty({ description: 'The unique identifier for the board' })
   @PrimaryGeneratedColumn()
   id: number;
 
+  @ApiProperty({ description: 'Name of the board' })
   @Column()
   name: string;
 
-  @OneToMany(() => BoardColumn, (boardColumns) => boardColumns.board)
+  @ApiProperty({
+    description: 'List of columns associated with the board',
+    type: () => BoardColumn,
+  })
+  @OneToMany(() => BoardColumn, (boardColumn) => boardColumn.board)
   boardColumns: BoardColumn[];
 
+  @ApiProperty({
+    description: 'List of users who are members of the board',
+    type: () => User,
+  })
   @ManyToMany(() => User, (user) => user.boards)
   members: User[];
 
+  @ApiProperty({
+    description: 'The user who owns the board',
+    type: () => User,
+  })
   @ManyToOne(() => User, (user) => user.id)
   owner: User;
 
+  @ApiProperty({ description: 'Creation date of the board' })
   @CreateDateColumn()
   date: Date;
 }
--- packages/server/src/board/board.module.ts
+++ packages/server/src/board/board.module.ts
@@ -8,9 +8,14 @@ import { Board } from './board.entity';
 import { User } from '../user/User.entity';
 import { BoardColumn } from '../column/column.entity';
 
+/**
+ * Module to group and provide board-related controllers, services and entities.
+ * It imports necessary TypeORM features for User, Board, and BoardColumn entities.
+ * It provides BoardService, UserService, and UserRepository to be used application-wide.
+ */
 @Module({
-  imports: [TypeOrmModule.forFeature([User, Board, BoardColumn])],
-  providers: [BoardService, UserService, UserRepository],
-  controllers: [BoardController],
+  imports: [TypeOrmModule.forFeature([User, Board, BoardColumn])], // Importing necessary entities
+  providers: [BoardService, UserService, UserRepository], // Providing services and repository for dependency injection
+  controllers: [BoardController], // Registering controller for handling board routes
 })
 export class BoardModule {}
